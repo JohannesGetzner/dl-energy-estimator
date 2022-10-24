c5e0cd1dc6e9c31c8b3deef1a6268c1daca54f24
JohannesGetzner <j.getzner@gmail.com>
2022-10-24 11:16:00 +0200

    added package __init__ file to data_collectors

---
+++ data_collectors/__init__.py
@@ -0,0 +1,4 @@
+from .conv2d import Conv2dDataCollector
+from .linear import LinearDataCollector
+from .maxpooling2d import MaxPooling2dDataCollector
+from .activations import ActivationsDataCollector
