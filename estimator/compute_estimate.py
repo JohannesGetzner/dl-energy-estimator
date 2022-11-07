f44d5ebf9b611fb2c4f324c3275b2512f4970cde
JohannesGetzner <j.getzner@gmail.com>
2022-11-07 13:58:25 +0100

    added docstrings to compute_estimate.py

--- estimator/compute_estimate.py
+++ estimator/compute_estimate.py
@@ -3,6 +3,13 @@ from .architecture_parser import parse_architecture
 
 
 def compute_energy_estimate(architecture: nn.Module, batch_size: int, config):
+    """
+    iterates over the channels and compute the energy estimate of each one
+    :param architecture: the architecture to compute the estimate for
+    :param batch_size: the batch_size of the forward pass
+    :param config: the configuration from 'estimation_config.yaml'
+    :return: the total estimate energy_consumption of the architecture and the channel wise estimates
+    """
     channels = parse_architecture(architecture=architecture, batch_size=batch_size, config=config)
     total_energy = 0
     channel_wise_energies = []
